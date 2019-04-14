814a47632ca3436a11bdc9985875b1f7291f2ba5
Squiro <alesio_sinopoli@hotmail.com>
2019-04-14 10:34:56 -0300

    Nuevos casos de prueba para FormandoEquipos, VendedorasPremiadas

--- Taller/CodeGolf/Operaciones/src/ar/edu/unlam/operaciones/Operaciones.java
+++ Taller/CodeGolf/Operaciones/src/ar/edu/unlam/operaciones/Operaciones.java
@@ -5,13 +5,13 @@ public class Operaciones {
 	public static void main(String[] args) {	
 		Operaciones op = new Operaciones();
 		
-		System.out.println(op.operaciones(2017));
+		System.out.println("Num OP: " + op.operaciones(2017));
 	}
 	
 	public int operaciones(int n) {
 		int o = 0;
 		while (n != 0) {
-			n = n%2 == 0 ? n/2 : n-1;
+			n = n%2 < 1 ? n/2 : n-1;
 			o++;
 		}
 		return o;
